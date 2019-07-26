55f17b720f9850084caa3cad03a260f36e124c68
steeb-wintec <steeb.webdev@gmail.com>
2019-07-26 21:51:39 +1200

    minor changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,13 +85,13 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
+                    <!-- <a href="http://steebthomas.com/">hello</a> -->
+                    <a href="http://steebthomas.com/contact">Contact</a>
+                    <a href="http://steebthomas.com/work">Work</a>
+                    <a href="http://steebthomas.com/blog">Blog</a>
+                    <!-- <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
                 </div>
             </div>
         </div>
